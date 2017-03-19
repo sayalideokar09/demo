71a54c5d7e2a43fd8302b3ed7bbf309eb5f36a0c
Sneha <Sneha@Sneha-PC>
2017-03-19 09:34:17 +0530

    my first commit

---
+++ collectionDemos/src/main/java/com/collection/collectionDemos/InvalideCollectionTypeForEnumaration.java
@@ -0,0 +1,15 @@
+package com.collection.collectionDemos;
+
+public class InvalideCollectionTypeForEnumaration extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalideCollectionTypeForEnumaration(String message) {
+		super(message);
+	}
+
+	
+}
